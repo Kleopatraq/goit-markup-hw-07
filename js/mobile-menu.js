6528dae7a6e95b3c4fa6a27e8ae6bf07d3c24dbe
Kleopatraq <126488857+Kleopatraq@users.noreply.github.com>
2023-04-30 03:18:13 +0300

    add-menu-mobile

---
+++ js/mobile-menu.js
@@ -0,0 +1,17 @@
+(() => {
+  const refs = {
+    openMuneBtn: document.querySelector("[.js-open-menu]"),
+    closeMuneBtn: document.querySelector("[js-close-menu]"),
+    menu: document.querySelector("[modile-menu]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleMenu);
+  refs.closeModalBtn.addEventListener("click", toggleMenu);
+  refs.modal.addEventListener("click", (click) => {
+    if (click.target === click.currentTarget) toggleModal();
+  });
+  function toggleMenu() {
+    refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle("no-scroll");
+  }
+})();
